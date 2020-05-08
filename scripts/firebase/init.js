8bdecb5e3b29a30f40e7fb60f0f34a7cce0b21ea
Caio Magalhães <caio.lmf@gmail.com>
2020-05-08 17:29:10 -0300

    Refactore firebase integration

---
+++ scripts/firebase/init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBA-_wGS9hu3Fgp7hpPw3bAm2qkVoyBdA",
+  authDomain: "trybe-review.firebaseapp.com",
+  databaseURL: "https://trybe-review.firebaseio.com",
+  projectId: "trybe-review",
+  storageBucket: "trybe-review.appspot.com",
+  messagingSenderId: "523070581907",
+  appId: "1:523070581907:web:67588ed7e7cdde3ed27166",
+  measurementId: "G-4LT555CK0M"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
