6be3e45c804ff2d52d707086db6796ab6a5262c1
Caio Magalhães <caio.lmf@gmail.com>
2020-05-09 15:52:31 -0300

    Change botton sheet to open on button click using JS

--- scripts/core.js
+++ scripts/core.js
@@ -15,8 +15,23 @@ const create = async (user, database) => {
   return true;
 };
 
+const showBottonSheet = () => {
+  const overlay = document.querySelector('.overlay');
+  const sheet = overlay.querySelector('.menu');
+  const sheetClose = overlay.querySelector('.sheet-close');
+  // Active the botton sheet and its overlay
+  overlay.classList.add('is-active');
+  sheet.classList.add('is-active');
+  sheetClose.classList.add('is-active');
+  // Create a event on the overlay to close the botton sheet
+  sheetClose.addEventListener('click', () => {
+    overlay.classList.remove('is-active');
+    sheet.classList.remove('is-active');
+  });
+};
+
 const createUser = async (form) => {
-  // Get user data from signin form
+  // Get user data from sign in form
   const user = {
     name: form.elements[0].value,
     slackUser: form.elements[1].value,
@@ -24,14 +39,19 @@ const createUser = async (form) => {
   };
   // Store user data on firebase
   const result = await create(user, usersDatabase);
-  // IF ok show sucess msg else show try again msg
+  // If ok show sucess msg else show try again msg
   if (result) {
     document.querySelector('#new-user-form').hidden = true;
   }
 };
 
 window.onload = () => {
+  const joinBtn = document.querySelector('.join-button');
   const newUserForm = document.querySelector('#new-user-form');
+
+  joinBtn.addEventListener('click', () => {
+    showBottonSheet();
+  });
   newUserForm.addEventListener('submit', (e) => {
     e.preventDefault();
     createUser(newUserForm);
